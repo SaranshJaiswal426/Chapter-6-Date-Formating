d1014115f7539c2b18de2cbbe6d0fbfc453f9002
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-25 15:05:52 +0530

    outputting date into string

--- outputting date.py
+++ outputting date.py
@@ -1,4 +1,8 @@
+#try outdatting of datetime object of string
 from datetime import datetime
 datetime_for_string = datetime(2016,10,1,0,0)
+
 datetime_string_format = '%b %d %Y, %H:%M:%S'
-datetime.strftime(datetime_for_string,datetime_string_format)
+datetime_for_string.strftime(datetime_string_format)
+
+
